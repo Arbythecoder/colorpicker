7ca40c043bf4a477c076b7e949d25eb9976f8e7d
Arbythecoder <houseofarby@gmail.com>
2022-12-09 13:11:12 +0100

    changed click buttons and id updates

---
+++ jsfolder/index.js
@@ -0,0 +1,22 @@
+// selcting the DOM elements
+const colors = [
+  "green",
+  "red",
+  "yellow",
+  "pink",
+  "rgba(133,122,200)",
+  "#f15025",
+];
+const myClick = document.getElementById("click");
+const myColor = document.querySelector(".collar");
+
+myClick.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  myColor.textContent = colors[randomNumber];
+});
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
